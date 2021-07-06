332c1920f211df9c2bec7e81d722d154a4e9ee5d
Usernow <usernow.82@gmail.com>
2021-07-06 12:02:30 -0500

    1-9 Clases/Objetos/Static/Final

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import static ui.UIMenu.*;
+
+public class Main {
+    public static void main(String [] Args ){
+        /***
+        Doctor myDoctor = new Doctor(); //Metodo constructor por defecto
+        myDoctor.name = "Alejandro Rodriguez";
+        myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.name = "Anahit Salgado";
+        myDoctor.showName();
+        myDoctor.showId();***/
+
+        showMenu();
+
+
+    }
+}
